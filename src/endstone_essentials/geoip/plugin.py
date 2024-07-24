0fe19329188b9e2f0e7b0a274b5b9d43ef2fd0fa
Vincent <magicdroidx@gmail.com>
2024-07-24 18:41:20 +0100

    Add listener and config

--- src/endstone_essentials/geoip/plugin.py
+++ src/endstone_essentials/geoip/plugin.py
@@ -1,9 +1,14 @@
 from endstone.plugin import Plugin
 
+from endstone_essentials.geoip.listener import EssentialsGeoIPPlayerListener
+
 
 class EssentialsGeoIP(Plugin):
     name = "EssentialsGeoIP"
     api_version = "0.4"
 
     def on_enable(self) -> None:
-        pass
+        listener = EssentialsGeoIPPlayerListener()
+        self.register_events(listener)
+        self.logger.info("This product includes GeoLite2 data created by MaxMind, "
+                         "available from https://www.maxmind.com/.")
